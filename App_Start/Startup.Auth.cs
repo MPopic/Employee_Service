23c7f47e7af0f38326ceaf4a386608624714dd95
MPopic <MPopic@users.noreply.github.com>
2020-09-29 15:52:15 +0200

    First commit

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using Employee_Service.Providers;
+using Employee_Service.Models;
+using Microsoft.Owin.Security.Facebook;
+using EmployeeService.Facebook;
+
+namespace Employee_Service
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static string PublicClientId { get; private set; }
+
+        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context and user manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Configure the application for OAuth based flow
+            PublicClientId = "self";
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new ApplicationOAuthProvider(PublicClientId),
+                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(10),
+                // In production mode set AllowInsecureHttp = false
+                AllowInsecureHttp = true
+            };
+
+            // Enable the application to use bearer tokens to authenticate users
+            app.UseOAuthBearerTokens(OAuthOptions);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            //    appId: "1448829408645793",
+            //    appSecret: "b5ac7d21c3e6441c8e5663cc40989b97");
+
+            var facebookOptions = new FacebookAuthenticationOptions()
+            {
+                AppId = "1448829408645793",
+                AppSecret = "b5ac7d21c3e6441c8e5663cc40989b97",
+                BackchannelHttpHandler = new FacebookBackChannelHandler(),
+                UserInformationEndpoint = "https://graph.facebook.com/v2.4/me?fields=id,email"
+            };
+            facebookOptions.Scope.Add("email");
+            app.UseFacebookAuthentication(facebookOptions);
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "496780769630-eqk3aun2oobtn4dr5paj8jjj9b5fsc8f.apps.googleusercontent.com",
+                ClientSecret = "NGfYuaLw7J_HDtvwk-slr8eA"
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Employee_Service
  The Web API Employee_Service will retrive and return Employees data
+ The Web API Employee_Service will retrive and return data from Employees table created in SQL Server. Entity Framework(ADO.NET Entity Data Model) was used to retrive data. Application has Login/Register functionality required to view Employees data. Social logins(Google, Facebook) are implemented.
